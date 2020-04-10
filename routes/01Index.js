17427d24bef0878196efe87c1095fc5e60784d60
BDFD <bdfd2005@gmail.com>
2020-04-11 01:38:20 +0800

    minor update

--- routes/01Index.js
+++ routes/01Index.js
@@ -6,10 +6,10 @@ var router = express.Router();
 //router address: /
 //descriptions: Index Page
 //comments: www.maplestoryinfo.tk
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('01Index/index', {
     // title: 'Entrance Page',
-    layout: 'page_layouts/index_layout'
+    layout: 'page_layouts/index_layout',
   });
 });
 
@@ -17,11 +17,27 @@ router.get('/', function(req, res, next) {
 //descriptions: Home Index Page
 //comments: Selection Page -> select the part to view details
 
-router.get('/home', function(req, res, next) {
+router.get('/home', function (req, res, next) {
   res.render('01Index/home', {
     // title: 'Entrance Page',
-    layout: 'page_layouts/home_layout'
+    layout: 'page_layouts/home_layout',
   });
+});
+
+//router address: /contact
+//descriptions: Contact Page
+//comments:
+
+router.get('/contact', function (req, res, next) {
+  res.render('01Index/contact');
+});
+
+//router address: /about
+//descriptions: About Page
+//comments:
+
+router.get('/about', function (req, res, next) {
+  res.render('01Index/about');
 });
 
 module.exports = router;
